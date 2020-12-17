16cab764c5fb8ca2f395295566651d44dfd0f963
Muhammad Faizan <faizansohail077@gmail.com>
2020-12-17 03:54:04 -0500

    Project 4b

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import ContentUx from './component/ContentUx';
+import Header from './component/Header';
+import Navbar from './component/Navbar';
+import ContentCopyWrite from './component/ContentCopyWrite'
+import ContentWriting from './component/ContentWriting';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>web animation</h1>
+      <Navbar/>
+      <Header/>
+      <ContentUx/>
+      <ContentCopyWrite/>
+      <ContentWriting/>
     </div>
   );
 }
